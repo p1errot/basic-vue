b24596e01fc4a2654c6d205e601dcecfab15d783
Samuel Rivera <samuelp1errot@gmail.com>
2019-12-19 15:33:53 -0500

    Getting a XML data and parsing

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,10 +1,43 @@
 <template>
-  <footer>Copyright &copy; 2019</footer>
+  <footer>
+    <div>
+      XML <span
+        v-for="(item, index) in itemsXML"
+        :key="index"
+      >
+        - {{item.YEAR._text}}
+      </span>
+    </div>
+    <p>Copyright &copy; 2019</p>
+  </footer>
 </template>
 
 <script>
+import axios from 'axios';
+import convert from 'xml-js';
+
 export default {
-  name: "PageFooter"
+  name: "PageFooter",
+  data() {
+    return {
+      itemsXML: []
+    }
+  },
+  created() {
+    this.getXML();
+  },
+  methods: {
+    getXML() {
+      axios
+        .get('//www.mocky.io/v2/5dfa935d2f00007400ff9a5e')
+        .then(this.handleXMLResponse);
+    },
+    handleXMLResponse(resp) {
+      const convertedData = convert.xml2js(resp.data, { compact: true });
+      const itemsXML = convertedData.CATALOG.CD.filter((el, index) => index < 5);
+      this.$set(this, 'itemsXML', itemsXML);
+    }
+  }
 };
 </script>
 
